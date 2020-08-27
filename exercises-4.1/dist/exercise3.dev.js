0e2437e414fe8103673a1ca584cda7c077427cef
Lucas Andrade <lafdesouza2002@outlook.com>
2020-08-27 20:02:15 -0300

    Exercise 3

---
+++ exercises-4.1/dist/exercise3.dev.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var a = 3;
+var b = 4;
+var c = 8;
+
+if (a > b && a > c) {
+  console.log(a);
+} else if (b > a && b > c) {
+  console.log(b);
+} else if (c > b && c > a) {
+  console.log(c);
+}
