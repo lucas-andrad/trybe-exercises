5572acf73922b9279a532f25c5985d0550b1201a
Lucas Andrade <lafdesouza2002@outlook.com>
2020-09-17 19:26:48 -0300

    Corrigindo erros

--- bloco_6/exercises-6.1/script.js
+++ bloco_6/exercises-6.1/script.js
@@ -1,8 +1,13 @@
 const arr = ["Acre", "Alagoas", "Amapá", "Amazonas", "Bahia", "Ceará", "Espírito Santo", "Goiás", "Maranhão", "Mato Grosso", "Mato Grosso do Sul", "Minas Gerais", "Pará", "Paraíba", "Pernambuco", "Piauí", "Rio de Janeiro", "Rio Grande do Norte", "Rio Grande do Sul", "Rondônia", "Roraima", "Santa Catarina", "São Paulo", "Sergipe", "Tocantins", "Distrito Federal"];
 const options = document.querySelector("#options");
+const form = document.querySelector("#formulario").elements
+const labels = document.querySelectorAll("label")
+
+console.log(form)
 
 for (let i = 0; i < arr.length; i += 1) {
   let option = document.createElement("option")
   option.innerText = arr [i]
   options.appendChild(option)  
 }
+
