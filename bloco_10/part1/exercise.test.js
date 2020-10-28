1182ef50436e84d0722e626d621633027870fbc2
Lucas Andrade <lafdesouza2002@outlook.com>
2020-10-28 19:17:11 -0300

    Exercise2.js

--- bloco_10/part1/exercise.test.js
+++ bloco_10/part1/exercise.test.js
@@ -1,8 +1,14 @@
-const sum = require('./exercise1.js')
+const { it, expect } = require('@jest/globals');
+const sum = require('./exercise1.js');
+const myRemove = require('./exercise2.js');
 
 describe('Testando se as funções passadas estão implementadas corretamente', () => {
-    it('Testando se as funções de soma estão sendo implementadas corretamente', () => {
-        expect(sum(4, 5)).toBe(9);
-        expect(sum(0, 0)).toBe(0);
-    })
-})
+  it('Testando se as funções de soma estão sendo implementadas corretamente', () => {
+    expect(sum(4, 5)).toBe(9);
+    expect(sum(0, 0)).toBe(0);
+  });
+  it('Testando se as funções de remover um item específico do array estão funcionando corretamente', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+    expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+  });
+});
