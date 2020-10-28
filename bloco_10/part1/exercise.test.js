328f0cf03928525658860fa548c5ef10577ea201
Lucas Andrade <lafdesouza2002@outlook.com>
2020-10-28 19:21:12 -0300

    Exercise3.js

--- bloco_10/part1/exercise.test.js
+++ bloco_10/part1/exercise.test.js
@@ -7,6 +7,10 @@ describe('Testando se as funções passadas estão implementadas corretamente', 
     expect(sum(4, 5)).toBe(9);
     expect(sum(0, 0)).toBe(0);
   });
+  it('Testando se as funções de retornar uma cópia sem um item específico do array estão funcionando corretamente', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+    expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+  });
   it('Testando se as funções de remover um item específico do array estão funcionando corretamente', () => {
     expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
     expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
