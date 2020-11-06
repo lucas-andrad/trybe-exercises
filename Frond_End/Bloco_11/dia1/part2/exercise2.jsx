8bd855b1ab7af8472de45ac6a1119dc64a1c26d7
Lucas Andrade <lafdesouza2002@outlook.com>
2020-11-05 21:30:57 -0300

    Exercise2 part2

---
+++ Frond_End/Bloco_11/dia1/part2/exercise2.jsx
@@ -0,0 +1,9 @@
+const JSX = <div>
+                <h1>Kanye West</h1>
+                <p>3 best musics</p>
+                <ul>
+                    <li>Runaway</li>
+                    <li>Gold Digger</li>
+                    <li>All Falls Down</li>
+                </ul>
+            </div>;
